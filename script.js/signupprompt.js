99d4cabd741aac58cf65f4d05944219b0c1034a5
Olatunbosun Adeoye <aolatunbosun11@gmail.com>
2024-08-30 03:00:29 +0100

    Added and Styled a Sign up Prompt Page

---
+++ script.js/signupprompt.js
@@ -0,0 +1,9 @@
+// Function to handle User Sign Up Prompt click
+document.getElementById("userRole").addEventListener("click", function () {
+  window.location.href = "https://www.google.com/";
+});
+
+// Function to handle Handy Man Signup Prompt click
+document.getElementById("handyManRole").addEventListener("click", function () {
+  window.location.href = "https://www.youtube.com/";
+});
