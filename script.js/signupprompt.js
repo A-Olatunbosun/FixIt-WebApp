0e864205659c6f2e383868554d486a584579794b
Olatunbosun Adeoye <aolatunbosun11@gmail.com>
2024-08-30 14:37:07 +0100

    Added and style the user and handyman sign up page

--- script.js/signupprompt.js
+++ script.js/signupprompt.js
@@ -1,9 +1,9 @@
 // Function to handle User Sign Up Prompt click
 document.getElementById("userRole").addEventListener("click", function () {
-  window.location.href = "https://www.google.com/";
+  window.location.href = "./signup.html";
 });
 
 // Function to handle Handy Man Signup Prompt click
 document.getElementById("handyManRole").addEventListener("click", function () {
-  window.location.href = "https://www.youtube.com/";
+  window.location.href = "./signup.html";
 });
